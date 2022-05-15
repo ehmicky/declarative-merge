05587f9ef9ce2ba8adbc9f3cfadc0d00caef21f3
ehmicky <ehmicky@gmail.com>
2022-05-15 17:34:50 +0200

    Add more comments

--- src/array.js
+++ src/array.js
@@ -2,6 +2,8 @@ import { set as setArray, test as isUpdatesObject } from 'set-array'
 
 // Test whether the `secondObject` is an array `updates` object like
 // `{ [index]: values, ... }`
+// Also confirms the `firstValue` is an array, so we are sure the user intended
+// to use an array `updates` object.
 export const shouldPatchArray = function (firstValue, secondObject) {
   return Array.isArray(firstValue) && isUpdatesObject(secondObject)
 }
