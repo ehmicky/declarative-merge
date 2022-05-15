3cec2be9ce9d70594f73a14e59581bfa6a22f00d
ehmicky <ehmicky@gmail.com>
2022-05-15 15:51:57 +0200

    Simplify test

--- test/own.js
+++ test/own.js
@@ -1,25 +1,20 @@
 import test from 'ava'
 import notDeepMerge from 'not-deep-merge'
+import { each } from 'test-each'
 
-test.serial(`Inherited properties are ignored in plain objects`, (t) => {
-  // eslint-disable-next-line no-extend-native, fp/no-mutation
-  Object.prototype.notOwn = 1
-  const second = { aa: 1 }
-  t.is(second.notOwn, 1)
-  const result = notDeepMerge({}, second)
-  t.is(result.notOwn, 1)
-  // eslint-disable-next-line fp/no-delete
-  delete Object.prototype.notOwn
-  t.false('notOwn' in result)
-})
-
-test(`Inherited properties are kept in other objects as part of the prototype`, (t) => {
-  const prototype = { notOwn: 1 }
-  const second = { aa: 1, __proto__: prototype }
-  t.is(second.notOwn, 1)
-  const result = notDeepMerge({}, second)
-  t.is(result.notOwn, 1)
-  // eslint-disable-next-line fp/no-delete
-  delete prototype.notOwn
-  t.false('notOwn' in result)
+each([Object.prototype, {}], ({ title }, prototype) => {
+  test.serial(
+    `Inherited properties are ignored in plain objects | ${title}`,
+    (t) => {
+      // eslint-disable-next-line fp/no-mutation, no-param-reassign
+      prototype.notOwn = 1
+      const second = { aa: 1, __proto__: prototype }
+      t.is(second.notOwn, 1)
+      const result = notDeepMerge({}, second)
+      t.is(result.notOwn, 1)
+      // eslint-disable-next-line fp/no-delete, no-param-reassign
+      delete prototype.notOwn
+      t.false('notOwn' in result)
+    },
+  )
 })
