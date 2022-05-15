4c356dcefa5a3cb09d98276c68a0413e863a5d97
ehmicky <ehmicky@gmail.com>
2022-05-15 17:35:36 +0200

    Merge files

--- test/enum.js
+++ test/enum.js
@@ -45,3 +45,20 @@ each(
     })
   },
 )
+
+each([Object.prototype, {}], ({ title }, prototype) => {
+  test.serial(
+    `Inherited properties are ignored in plain objects | ${title}`,
+    (t) => {
+      // eslint-disable-next-line fp/no-mutation, no-param-reassign
+      prototype.notOwn = 1
+      const second = { aa: 1, __proto__: prototype }
+      t.is(second.notOwn, 1)
+      const result = partialMerge({}, second)
+      t.is(result.notOwn, 1)
+      // eslint-disable-next-line fp/no-delete, no-param-reassign
+      delete prototype.notOwn
+      t.false('notOwn' in result)
+    },
+  )
+})
