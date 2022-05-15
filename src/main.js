0f82bcb067b36ae4d35286e29ce296a6ad226349
ehmicky <ehmicky@gmail.com>
2022-05-15 15:34:29 +0200

    Improve implementation

--- src/main.js
+++ src/main.js
@@ -1,14 +1,27 @@
 import isPlainObj from 'is-plain-obj'
+import { set as setArray, test as isArrayPatch } from 'set-array'
 
 // Deeply merge objects.
 // Inherited and non-enumerable properties are ignored.
 // Symbol properties are kept.
 // Plain objects are deeply cloned. Non-plain objects (and their children) are
 // not cloned.
+// If the second argument is an object with a property `_set: true`, the first
+// argument is overridden instead of being merged to.
+// If the second argument is an array patch object (like `{ 1: 'a', 3: 'f' }`),
+// the first argument is patched as an array. If it is not an array, an empty
+// array is used instead.
+// `_set` and patch objects are only allowed in the second argument:
+//  - They are considered normal properties in first argument
+//     - Reason: they would not make sense since the first argument has lower
+//       priority
+//  - If the first argument might use those formats, `not-deep-merge` should be
+//    applied to it first, using an empty object as first argument.
 export default function notDeepMerge(firstValue, secondValue) {
   return deepMerge(firstValue, secondValue, false)
 }
 
+// eslint-disable-next-line complexity
 const deepMerge = function (firstValue, secondValue) {
   if (!isPlainObj(secondValue)) {
     return secondValue
@@ -16,6 +29,10 @@ const deepMerge = function (firstValue, secondValue) {
 
   if (shouldSet(secondValue)) {
     return deepCloneSet(secondValue)
+  }
+
+  if (shouldPatchArray(secondValue)) {
+    return patchArray(firstValue, secondValue)
   }
 
   if (!isPlainObj(firstValue)) {
@@ -39,6 +56,15 @@ const deepCloneSet = function (object) {
 
 const deepClone = function (object) {
   return deepMerge({}, object)
+}
+
+const shouldPatchArray = function (object) {
+  return isArrayPatch(object) && Object.keys(object).length !== 0
+}
+
+const patchArray = function (firstValue, updates) {
+  const array = Array.isArray(firstValue) ? firstValue : []
+  return setArray(array, updates, { merge: deepMerge })
 }
 
 const deepMergeObjects = function (firstObject, secondObject) {
