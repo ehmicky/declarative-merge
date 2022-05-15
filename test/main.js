75084f6969e0a4433a54da6afd6e8721610185e9
ehmicky <ehmicky@gmail.com>
2022-05-15 15:42:38 +0200

    Add more tests

--- test/main.js
+++ test/main.js
@@ -2,6 +2,15 @@ import test from 'ava'
 import notDeepMerge from 'not-deep-merge'
 import { each } from 'test-each'
 
+const getNonEnumObj = function () {
+  const object = { enum: 1 }
+  // eslint-disable-next-line fp/no-mutating-methods
+  Object.defineProperty(object, 'nonEnum', { value: 1, enumerable: false })
+  return object
+}
+
+const nonEnumObj = getNonEnumObj()
+
 each(
   [
     { firstValue: { aa: 1 }, secondValue: { bb: 2 }, result: { aa: 1, bb: 2 } },
@@ -23,6 +32,31 @@ each(
     { firstValue: [1], secondValue: [2], result: [2] },
     { firstValue: { aa: 1 }, secondValue: [2], result: [2] },
     { firstValue: [1], secondValue: { aa: 1 }, result: { aa: 1 } },
+    {
+      firstValue: { aa: 1 },
+      secondValue: nonEnumObj,
+      result: { aa: 1, enum: 1 },
+    },
+    {
+      firstValue: nonEnumObj,
+      secondValue: { aa: 1 },
+      result: { aa: 1, enum: 1 },
+    },
+    {
+      firstValue: { aa: 1, nonEnum: 2 },
+      secondValue: nonEnumObj,
+      result: { aa: 1, enum: 1, nonEnum: 2 },
+    },
+    {
+      firstValue: nonEnumObj,
+      secondValue: { aa: 1, nonEnum: 2 },
+      result: { aa: 1, enum: 1, nonEnum: 2 },
+    },
+    {
+      firstValue: {},
+      secondValue: { aa: nonEnumObj },
+      result: { aa: { enum: 1 } },
+    },
   ],
   ({ title }, { firstValue, secondValue, result }) => {
     test(`notDeepMerge() result | ${title}`, (t) => {
