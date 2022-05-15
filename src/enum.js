b558760aaf37f6160192ed7651b04468fe8c77c5
ehmicky <ehmicky@gmail.com>
2022-05-15 17:34:33 +0200

    Add comment

--- src/enum.js
+++ src/enum.js
@@ -1,3 +1,5 @@
+// Retrieve object keys, including symbols, but excluding inherited and
+// non-enumerable properties
 export const getEnumKeys = function (object) {
   const keys = Object.keys(object)
   const symbols = Object.getOwnPropertySymbols(object)
@@ -6,12 +8,15 @@ export const getEnumKeys = function (object) {
     : [...keys, ...symbols.filter((symbol) => isEnum(object, symbol))]
 }
 
+// When merging an own enumerable property from `secondObject` but that property
+// is inherited or non-enumerable in `firstObject`, we use `undefined` instead
 export const getEnumValue = function (object, key) {
   return isEnum(object, key) ? object[key] : undefined
 }
 
 const { propertyIsEnumerable: isEnumerable } = Object.prototype
 
+// Test if a property is own and enumerable
 export const isEnum = function (object, propName) {
   return isEnumerable.call(object, propName)
 }
